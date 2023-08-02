b8819073afa2c17e35b37380d016a5236c21e401
Bilegdemberel Khurelchuluun <bilg21@yahoo.com>
2023-08-01 17:15:01 -0700

    nvim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
-require("bilg.base") 			-- General Settings
-require("bilg.plugins") 		-- Plugins
-require("bilg.highlights")	 	-- Colourscheme and other highlights
-require("bilg.remaps") 			-- Keymaps
-require("bilg.bootstrap") 		-- Packer Auto-Installer
-
----Pretty print lua table
-function _G.dump(...)
-	local objects = vim.tbl_map(vim.inspect, { ...})
-	print(unpack(objects))
-end
-	
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
